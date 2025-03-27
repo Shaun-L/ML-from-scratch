3a9e07723af1f7fa6fb19da1a9e94b3f25c7061a
Shaun Lim <shaunlim1214@gmail.com>
2025-03-27 13:38:24 -0700

    learned jax.jit

--- syntax-training/jax-tutorial.py
+++ syntax-training/jax-tutorial.py
@@ -1,26 +1,59 @@
 import jax.numpy as jnp
 import jax
 import os
+	
+def test1():
+    def f(x):
+	    return jnp.sin(x) + x**3 
 
-def f(x):
-    return jnp.sin(x) + x**3 
+    x = jnp.array(jnp.pi)
+    print(f"this is a jnp.array {x}")
+    y = f(x)
 
-x = jnp.array(jnp.pi)
-print(f"this is a jnp.array {x}")
-y = f(x)
+    print(f"f(2) = {y}")
 
-print(f"f(2) = {y}")
+    # Computes the Derivative
+    dfdx = jax.grad(f)
+    print(f"df/dx at x=2: {dfdx(x)}")
 
-# Computes the Derivative
-dfdx = jax.grad(f)
-print(f"df/dx at x=2: {dfdx(x)}")
-
-# Computes the second derivative
-d2fdx2 = jax.grad(dfdx)
-print(f"Second derivative at x=2: {d2fdx2(x)}")
+    # Computes the second derivative
+    d2fdx2 = jax.grad(dfdx)
+    print(f"Second derivative at x=2: {d2fdx2(x)}")
 
 
+def jitTutorial():
+    import time
+    
+    def slow_function(x):
+        return jnp.exp(x) + jnp.cos(x)
+    
+    # JIT-compiled version of the 'slow_function'
+    fast_function = jax.jit(slow_function)
 
+    # Input 
+    x = jnp.linspace(-10, 10, 100000000) # Large input for Benchmarking
+     
+    # Benchamrking w/out jit
+    start = time.time()
+    y_slow = slow_function(x).block_until_ready()
+    end = time.time()
+    noJit = end-start
+    print(f"Without jit: {noJit}")
+
+    # Benchmarkng w/ jit
+    start = time.time()
+    y_fast = fast_function(x).block_until_ready()
+    end = time.time()
+    wJit = end-start
+    print(f"With jit: {wJit}")
+    
+    print(f"The function ran {noJit/wJit} times faster w/ jit")
+    
+
+def main():
+    jitTutorial()
+    
+main()
 
 
 
